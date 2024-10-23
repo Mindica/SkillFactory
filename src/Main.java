83f1f85fc9635367f3db834a119f49f7e99575ef
Mindica <twilightflavor@gmail.com>
2024-10-23 20:25:49 +0300

    Test Commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Mage mage1 = new Mage("Dana", "fire", 2, 3);
+        Mage mage1 = new Mage("Dana", "fire", 3, 3);
         Mage mage2 = new Mage("Dima", "fire", 2, 3);
         mage1.fight(mage2);
         mage1.getInfo();
