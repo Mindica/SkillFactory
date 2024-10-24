1924ed16a8c3b9c911e9d899fa80b8ea3669ed58
Mindica <twilightflavor@gmail.com>
2024-10-24 19:01:49 +0300

    Test Commit 2

--- src/Mage.java
+++ src/Mage.java
@@ -19,6 +19,7 @@ public class Mage {
         String result = "";
         if (this.type.equals("fire") && mage.type.equals("ice")) {
             result = this.name;
+            System.out.println("Test");
         }
         if (this.type.equals("ice") && mage.type.equals("fire")) {
             result = mage.name;
